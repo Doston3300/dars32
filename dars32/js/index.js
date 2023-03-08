7b24f1143ed95ba03ae2c055860605ce4e84d8ab
Doston3300 <113879352+Doston3300@users.noreply.github.com>
2023-03-08 17:48:35 +0500

    Add files via upload

---
+++ dars32/js/index.js
@@ -0,0 +1,80 @@
+// const myArray = [125, 555, 44]
+
+// const calcNumber = function(number) {
+//     if (number >= 50 && number <= 300) {
+//         return number * 0.15;
+//     } else {
+//         return number * 0.3
+//     }
+
+// }
+
+
+// console.log(calcNumber(myArray[0]));
+// console.log(calcNumber(myArray[1]));
+// console.log(calcNumber(myArray[2]));
+
+
+
+
+
+
+
+// const myArray = [1998, 1999, 2000, 1997]
+// const myAge = function(year) {
+//     if (year <= 2000) {
+//         return 'siz 20 asirda tugilgansz'
+//     } else {
+//         return 'siz 21 asirda tugilgansz'
+//     }
+
+// }
+// console.log(myAge(myArray[0]));
+// console.log(myAge(myArray[1]));
+// console.log(myAge(myArray[2]));
+// console.log(myAge(myArray[3]));
+// console.log(myAge(myArray[4]));
+
+
+
+
+// function calc(number) {
+//     let juft = []
+//     let toq = []
+//     for (let i = 2; i <= 100; i++) {
+//         if (i % 2 == 0)
+//             juft.push(i)
+//         if (i % 2 == 1)
+//             toq.push(i)
+//     }
+// }
+
+
+
+
+
+
+// vazifa_1
+
+// const yil = [1985, 1995, 2000, 2004, 1950]
+// let year = 2023;
+
+// function myFunction(number) {
+//     return (year - number);
+// }
+// for (let i = 0; i < yil.length; i++) {
+//     console.log(myFunction(yil[i]));
+// }
+
+
+
+
+// vazifa_2   
+
+// const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// function myFunction(number) {
+//     return number * 2;
+// }
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log(myFunction(myArray[i]));
+// }
